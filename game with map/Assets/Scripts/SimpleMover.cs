97ee40154d98769da338823cae8269c6e83d9bcc
netanel <orez132@gmail.com>
2020-04-26 20:06:07 +0300

    ver 1.1

---
+++ game with map/Assets/Scripts/SimpleMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMover : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1*Time.deltaTime*speed,0,0);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3((-1)*Time.deltaTime*speed,0,0);
+        }
+    }
+}
